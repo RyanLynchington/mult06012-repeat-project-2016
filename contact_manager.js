b3218c4c9820a68a312bcc53e2cf9578a33e70a8
RyanLynchington <giggsyryan@hotmail.com>
2016-08-18 21:55:42 +0100

    Adding First Name
    This code allows me to add the first name of someone into my finished
    table.

--- contact_manager.js
+++ contact_manager.js
@@ -5,4 +5,12 @@ function mySubmit() {
 	var myDetails = document.getElementById("contactManagerTable");
 	var myNewRow = myDetails.insertRow(-1);
 	
+	var myFirstName = document.getElementById("fName").value;
+	var firstNameText = "";
+	var firstNameHere;
+	var myNameCell = myNewRow.insertCell(0);
+		for (firstNameHere in myFirstName) {
+			firstNameText += myFirstName[firstNameHere] + "";
+		myNameCell.innerHTML = firstNameText;
+			}
 }
